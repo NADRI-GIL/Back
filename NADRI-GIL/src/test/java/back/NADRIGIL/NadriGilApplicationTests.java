521335f9319872f0ac39a61cc1bd02e4b8463db1
eunseo <qnrqjrm03@naver.com>
2022-09-05 22:20:57 +0900

    feat : CORS 정책 설정, application.yml url 주소 & build.gradle 버전 업데이트

--- NADRI-GIL/src/test/java/back/NADRIGIL/NadriGilApplicationTests.java
+++ NADRI-GIL/src/test/java/back/NADRIGIL/NadriGilApplicationTests.java
@@ -1,13 +1,13 @@
-package back.NADRIGIL;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NadriGilApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package back.NADRIGIL;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NadriGilApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
